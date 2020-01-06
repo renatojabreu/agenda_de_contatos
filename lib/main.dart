5c92e2e97cef1daab12824a6e5ed8f8c5446bb8d
renatojabreu <renatoj_abreu@hotmail.com>
2020-01-06 14:04:20 -0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:agenda_de_contatos/ui/contact_page.dart';
+import 'package:flutter/material.dart';
+import 'ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
